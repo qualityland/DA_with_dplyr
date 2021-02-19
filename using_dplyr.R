9d9aa8b97e4f1ceb3e583d6c7ab51e2cc393e8aa
Stefan Schmidt <sschmidt@mbp11.local>
2021-02-19 17:59:01 +0100

    gapminder 2007 plot visualizing four variables.

--- using_dplyr.R
+++ using_dplyr.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(gapminder)
+library(ggplot2)
 
 head(gapminder)
 
@@ -7,3 +8,12 @@ gapminder %>%
   filter(continent == "Europe",
          country %in% c("Germany", "Switzerland"),
          year == 2007)
+gm2007 <- gapminder %>% 
+  filter(year == 2007)
+
+ggplot(data = gm2007) +
+  geom_point(mapping = aes(
+    x = gdpPercap,
+    y = lifeExp,
+    color = continent,
+    size = pop))
